8ef0b7574fce81ec36e9b76b4d2d5fb7eeb425dd
zhaojian <zltqzj@163.com>
2019-04-25 20:58:17 +0800

    Add testng

--- src/test/java/com/imooc/demo/TestClassDemo.java
+++ src/test/java/com/imooc/demo/TestClassDemo.java
@@ -4,11 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestClassDemo {
-    @Test
-    public void testcase1(){
-        Assert.assertTrue(false);
-        System.out.println("testcase1");
-    }
+    
     @Test
     public void testcase2(){
         Assert.assertTrue(true);
